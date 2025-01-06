d7a80e4be0e245ddc2a50844b61045c65ca49b84
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2025-01-06 21:56:15 +0600

    Should try added at home

---
+++ src/Components/Home/ShouldTry.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import HeadingTitle from "../Share/HeadingTitle";
+
+const ShouldTry = () => {
+    const [menus, setMenu] = useState([])
+
+    useEffect(() => {
+        fetch('/menu.json')
+            .then(res => res.json())
+            .then(data => setMenu(data))
+    }, [])
+
+    // console.log(menu)
+
+
+    return (
+        <div className='m-20'>
+
+            <HeadingTitle
+                heading={'CHEF RECOMMENDS'}
+                subHeading={' Should Try '} />
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+                {menus.filter(item => item.category == 'salad').slice(0, 3).map(menu => <div key={menu._id}>
+                    <div className=" bg-base-100 shadow-md">
+                        <figure>
+                            <img
+                                src={menu.image}
+                                alt="item" />
+                        </figure>
+                        <div className="p-5 bg-gray-100">
+                            <h2 className="text-xl font-semibold my-2 text-center">{menu.name}</h2>
+                            <p>{menu.recipe}</p>
+                            <div className="card-actions justify-center mt-5">
+                                <button className="border-b-4 border-black px-5 py-2 rounded-full shadow bg-gray-200 hover:bg-slate-800 hover:border-yellow-600 hover:text-white uppercase text-sm font-semibold">add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>)}
+            </div>
+        </div>
+    );
+};
+
+export default ShouldTry;
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -3,6 +3,7 @@ import Banner from '../Components/Home/Banner';
 import SwiperHome from '../Components/Home/SwiperHome';
 import BitroBoss from '../Components/Home/BitroBoss';
 import HomeMenu from '../Components/Home/HomeMenu';
+import ShouldTry from '../Components/Home/ShouldTry';
 
 const HomeLayout = () => {
     return (
@@ -16,11 +17,12 @@ const HomeLayout = () => {
 
             <HomeMenu />
 
-
             {/* --------------------------call us section---------------------- */}
-            <div className='p-20 text-center text-white text-4xl font-semibold bg-black mb-16 italic'>
+            <div className='p-20 text-center text-white text-4xl font-semibold bg-black mb-16 italic mx-16'>
                 <h1>Call Us: +88 0192345678910</h1>
             </div>
+
+            <ShouldTry />
 
 
         </div>
